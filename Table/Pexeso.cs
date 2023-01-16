26ddff042292729b5818e94b0f3909d2db3c113a
Martin <martin.rostejnsky@email.cz>
2023-01-16 20:28:09 +0100

    struktura

---
+++ Table/Pexeso.cs
@@ -0,0 +1,75 @@
+namespace Table
+{
+    public partial class Pexeso : Form
+    {
+        public Pexeso()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox14_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox15_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox16_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
