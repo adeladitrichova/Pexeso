104e48d9797f1352996f89117ebaf2a949e769d5
Adéla Ditrichová <Adela.ditrichova@gmx.com>
2023-01-25 13:46:59 +0100

    pandi counter! ja a labelo

--- Table/Pexeso.Designer.cs
+++ Table/Pexeso.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pexeso));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,17 +44,28 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
             // Pexeso
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(404, 381);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Pexeso";
             this.Text = "T";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -61,5 +73,6 @@
 
         private SplitContainer splitContainer1;
         private PictureBox pictureBox1;
+        private Label label1;
     }
 }
--- Table/Pexeso.cs
+++ Table/Pexeso.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Drawing;
 
 namespace Table
 {
     public partial class Pexeso : Form
     {
+        int count = 0;
         protected bool pandi = false;
         public Pexeso()
         {
@@ -12,24 +14,28 @@ namespace Table
 
         private void pictureBox1_Click_1(object sender, EventArgs e)
         {
-            if(pandi)
+        
+
+            if (pandi)
             {
                 Random rnd = new Random();
                 this.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
-
                 pandi = false;
-            } else
+            }
+            else
             {
                 Random rnd = new Random();
                 pandi = true;
-                this.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)); //this. = okno
-                  // pictureBoxN = picture box 300 iq ja vim
+                this.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)); //this. = okno                                                                                   // pictureBoxN = picture box 300 iq ja vim
+
             }
-            pictureBox1.Location = new Point(Random.Shared.Next(1, this.Size.Width-50), Random.Shared.Next(1, this.Size.Height-50));
 
-
-
+            pictureBox1.Location = new Point(Random.Shared.Next(1, this.Size.Width - 100), Random.Shared.Next(1, this.Size.Height - 100));
+            count++;
+            label1.Text = "už jsi chytil: "+count.ToString()+ ((count > 1) ? ((count > 4) ? " pand" : " pandi") : " pandu");
         }
+
+
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
